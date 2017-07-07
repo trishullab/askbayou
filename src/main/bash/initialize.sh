28e5c9946526eb16a0f38e7c4c6b77de83d57a52
Ubuntu <ubuntu@ip-172-30-0-191.ec2.internal>
2017-07-07 22:54:02 +0000

    fix: chmod scripts to +x

--- src/main/bash/initialize.sh
+++ src/main/bash/initialize.sh
@@ -12,6 +12,7 @@ sudo -u askbayou sh -c "unzip /home/askbayou/bayou-1.0.0-ubuntu-16.01.zip -d /ho
 sudo -u askbayou sh -c "rm -f /home/askbayou/bayou-1.0.0-ubuntu-16.01.zip"
 sudo -u askbayou sh -c "cp start_ask_bayou.sh /home/askbayou/start_ask_bayou.sh"
 sudo -u askbayou sh -c "cp ../log4j/apiSynthesisServerLog4j2.xml /home/askbayou/resources/conf/apiSynthesisServerLog4j2.xml"
+sudo -u askbayou sh -c "chmod +x /home/askbayou/*.sh"
 
 cp ../systemd/askbayou.service /etc/systemd/system/
 systemctl enable askbayou.service
