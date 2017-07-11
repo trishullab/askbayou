1fa175bcbc8dfa2d8a9db726638f9acafd13ed6f
Ubuntu <ubuntu@ip-172-30-0-210.ec2.internal>
2017-07-11 22:29:44 +0000

    add fstab mounting of efs

--- src/main/bash/initialize.sh
+++ src/main/bash/initialize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install unzip
+apt-get install unzip, nfs-common
 
 adduser --gecos '' --disabled-password askbayou
 
@@ -13,6 +13,9 @@ sudo -u askbayou sh -c "rm -f /home/askbayou/bayou-1.0.0-ubuntu-16.01.zip"
 sudo -u askbayou sh -c "cp start_ask_bayou.sh /home/askbayou/start_ask_bayou.sh"
 sudo -u askbayou sh -c "cp ../log4j/apiSynthesisServerLog4j2.xml /home/askbayou/resources/conf/apiSynthesisServerLog4j2.xml"
 sudo -u askbayou sh -c "chmod +x /home/askbayou/*.sh"
+sudo -u askbayou sh -c "mkdir /home/askbayou/efs_logs"
+
+cp ../fstab/fstab /etc/fstab
 
 cp ../systemd/askbayou.service /etc/systemd/system/
 systemctl enable askbayou.service
