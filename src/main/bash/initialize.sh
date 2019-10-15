357b2aa198f024dbb4dac7997441126c2429a81d
Matt Barnett <barnett@rice.edu>
2019-10-15 00:04:07 -0500

    Update initialize.sh
    Update to bayou 1.1.0

--- src/main/bash/initialize.sh
+++ src/main/bash/initialize.sh
@@ -7,9 +7,9 @@ adduser --gecos '' --disabled-password askbayou
 
 wget http://release.askbayou.com/bayou-1.0-ubuntu-16.01.zip
 
-mv bayou-1.0-ubuntu-16.01.zip /home/askbayou/bayou-1.0-ubuntu-16.01.zip
-sudo -u askbayou sh -c "unzip /home/askbayou/bayou-1.0-ubuntu-16.01.zip -d /home/askbayou"
-sudo -u askbayou sh -c "rm -f /home/askbayou/bayou-1.0-ubuntu-16.01.zip"
+mv bayou-1.0-ubuntu-16.01.zip /home/askbayou/bayou-1.1.0-ubuntu-16.01.zip
+sudo -u askbayou sh -c "unzip /home/askbayou/bayou-1.1.0-ubuntu-16.01.zip -d /home/askbayou"
+sudo -u askbayou sh -c "rm -f /home/askbayou/bayou-1.1.0-ubuntu-16.01.zip"
 sudo -u askbayou sh -c "cp start_ask_bayou.sh /home/askbayou/start_ask_bayou.sh"
 sudo -u askbayou sh -c "cp ../log4j/apiSynthesisServerLog4j2.xml /home/askbayou/resources/conf/apiSynthesisServerLog4j2.xml"
 
